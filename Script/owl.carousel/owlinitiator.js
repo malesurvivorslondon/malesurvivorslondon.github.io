d63ddbd8ef4f1c741db3a8061ca0a93f94b95fdc
Saifeddine Chagdali <sifth3pro@gmail.com>
2021-05-19 13:38:48 +0100

    Add files via upload

---
+++ Script/owl.carousel/owlinitiator.js
@@ -0,0 +1,28 @@
+﻿        $(document).ready(function () {
+            $('.owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                        loop: true,
+                        nav: false,
+                        autoplay: true
+                    },
+                    600: {
+                        items: 1,
+                        loop: true,
+                        nav: false,
+                        autoplay: true
+                    },
+                    1000: {
+                        items: 1,
+                        nav: false,
+                        loop: true,
+                        autoplay: true,
+                        margin: 20
+                    }
+                }
+            })
+        })
